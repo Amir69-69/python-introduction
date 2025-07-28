0d7fbc75c253f3f29147ea1668618b3ab0b891f8
AMIR <jeanpauldacruz2@gmail.com>
2025-07-28 19:30:44 +0200

    reverse_string

--- exercises/4-fizzbuzz/src/fizzbuzz.py
+++ exercises/4-fizzbuzz/src/fizzbuzz.py
@@ -1,2 +1,22 @@
 def fizzbuzz(number: int) -> str:
-    return str(number)
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
+    else:
+        return str(number)
+
+def fizzbuzz(number: int) -> str:
+    is_fizz = number % 3 == 0 or '3' in str(number)
+    is_buzz = number % 5 == 0 or '5' in str(number)
+
+    if is_fizz and is_buzz:
+        return "FizzBuzz"
+    elif is_fizz:
+        return "Fizz"
+    elif is_buzz:
+        return "Buzz"
+    else:
+        return str(number)
--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -1,4 +1,37 @@
-from src import fizzbuzz
+from src.fizzbuzz import fizzbuzz
 
-# def test_fizzbuzz():
-#     assert fizzbuzz(1)
+def test_classic_number():
+    assert fizzbuzz(1) == "1"
+    assert fizzbuzz(2) == "2"
+    assert fizzbuzz(4) == "4"
+
+def test_fizz_only():
+    assert fizzbuzz(3) == "Fizz"
+    assert fizzbuzz(6) == "Fizz"
+
+def test_buzz_only():
+    assert fizzbuzz(5) == "Buzz"
+    assert fizzbuzz(10) == "Buzz"
+
+def test_fizzbuzz():
+    assert fizzbuzz(15) == "FizzBuzz"
+    assert fizzbuzz(30) == "FizzBuzz"
+
+def test_contains_three():
+    assert fizzbuzz(13) == "Fizz"
+    assert fizzbuzz(31) == "Fizz"
+    assert fizzbuzz(33) == "Fizz"  # contient 3 ET % 3
+
+def test_contains_five():
+    assert fizzbuzz(52) == "Buzz"
+    assert fizzbuzz(59) == "Buzz"
+    assert fizzbuzz(55) == "Buzz"
+
+def test_contains_both():
+    assert fizzbuzz(35) == "FizzBuzz"
+    assert fizzbuzz(53) == "FizzBuzz"
+    assert fizzbuzz(15) == "FizzBuzz"  # %3 et %5
+
+def test_neither():
+    assert fizzbuzz(7) == "7"
+    assert fizzbuzz(8) == "8"
