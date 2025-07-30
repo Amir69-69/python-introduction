9f323f156b2e2747f4abf66a161ec3292c087c13
AMIR <jeanpauldacruz2@gmail.com>
2025-07-30 21:11:09 +0200

    Bowling avec les class

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -1,39 +1,50 @@
-def compute_score(game: str) -> int:
-    """
-    Calcule le score total d'une partie de bowling.
-    :param game: Une chaîne représentant les lancers (ex : "X X X X X X X X X X X X")
-    :return: Score total
-    """
-    rolls = []
-    i = 0
-    while i < len(game):
-        if game[i] == 'X':
-            rolls.append(10)
-            i += 1
-        elif game[i] == '/':
-            rolls.append(10 - rolls[-1])
-            i += 1
-        elif game[i] == '-':
-            rolls.append(0)
-            i += 1
-        elif game[i] == ' ':
-            i += 1
-        else:
-            rolls.append(int(game[i]))
-            i += 1
+# exercises/6-bowling/src/bowling.py
 
-    score = 0
-    roll_index = 0
-    for frame in range(10):
-        if rolls[roll_index] == 10:  # strike
-            score += 10 + rolls[roll_index + 1] + rolls[roll_index + 2]
-            roll_index += 1
-        elif sum(rolls[roll_index:roll_index + 2]) == 10:  # spare
-            score += 10 + rolls[roll_index + 2]
-            roll_index += 2
-        else:
-            score += sum(rolls[roll_index:roll_index + 2])
-            roll_index += 2
-    return score
+class InvalidFrameError(Exception):
+    pass
 
+class TooManyRollsError(Exception):
+    pass
 
+class BowlingGame:
+    def __init__(self):
+        self.rolls = []
+
+    def lancer(self, quilles):
+        if quilles < 0 or quilles > 10:
+            raise ValueError("Le nombre de quilles doit être entre 0 et 10")
+
+        self.rolls.append(quilles)
+
+    def score(self):
+        total = 0
+        i = 0
+        for frame in range(10):
+            if self._is_strike(i):
+                if i + 2 >= len(self.rolls):
+                    raise TooManyRollsError("Pas assez de lancers pour calculer le bonus du strike")
+                total += 10 + self.rolls[i + 1] + self.rolls[i + 2]
+                i += 1
+            elif self._is_spare(i):
+                if i + 2 >= len(self.rolls):
+                    raise TooManyRollsError("Pas assez de lancers pour calculer le bonus du spare")
+                total += 10 + self.rolls[i + 2]
+                i += 2
+            else:
+                if i + 1 >= len(self.rolls):
+                    raise TooManyRollsError("Pas assez de lancers pour un frame normal")
+                frame_score = self.rolls[i] + self.rolls[i + 1]
+                if frame_score > 10:
+                    raise InvalidFrameError("Un frame ne peut pas dépasser 10 quilles")
+                total += frame_score
+                i += 2
+        return total
+
+    def _is_strike(self, i):
+        return i < len(self.rolls) and self.rolls[i] == 10
+
+    def _is_spare(self, i):
+        return (
+            i + 1 < len(self.rolls) and
+            self.rolls[i] + self.rolls[i + 1] == 10
+        )
--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -1,14 +1,50 @@
-from src.bowling import compute_score
-
-def test_perfect_game():
-    assert compute_score("X X X X X X X X X X X X") == 300
-
-def test_all_nines():
-    assert compute_score("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-") == 90
-
-def test_all_spares_with_5():
-    assert compute_score("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5") == 150
+# exercises/6-bowling/test_bowling.py
 
 
+from src.bowling import BowlingGame, InvalidFrameError, TooManyRollsError
 
+def test_1_score_all_zeros():
+    game = BowlingGame()
+    for _ in range(20):
+        game.lancer(0)
+    assert game.score() == 0
 
+def test_2_score_all_ones():
+    game = BowlingGame()
+    for _ in range(20):
+        game.lancer(1)
+    assert game.score() == 20
+
+def test_3_one_spare():
+    game = BowlingGame()
+    game.lancer(5)
+    game.lancer(5)  # spare
+    game.lancer(3)
+    for _ in range(17):
+        game.lancer(0)
+    assert game.score() == 16  # 10 + 3
+
+def test_4_one_strike():
+    game = BowlingGame()
+    game.lancer(10)  # strike
+    game.lancer(3)
+    game.lancer(4)
+    for _ in range(16):
+        game.lancer(0)
+    assert game.score() == 24  # 10 + 3 + 4 + 3 + 4
+
+def test_5_perfect_game():
+    game = BowlingGame()
+    for _ in range(12):  # 12 strikes
+        game.lancer(10)
+    assert game.score() == 300
+
+def test_6_invalid_frame_too_many_quilles():
+    game = BowlingGame()
+    game.lancer(8)
+    game.lancer(5)
+    try:
+        game.score()  # C’est ici que l’erreur doit se produire
+        assert False, "Devrait lever une exception"
+    except InvalidFrameError:
+        pass
